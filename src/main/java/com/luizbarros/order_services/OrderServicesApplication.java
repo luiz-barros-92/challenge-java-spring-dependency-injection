45adf88662c262baa9488ad120bfdf05d4c3bdd1
Luiz Barros <lck.barros@gmail.com>
2025-05-24 19:43:33 -0300

    Starting the challenge, initial configuration

---
+++ src/main/java/com/luizbarros/order_services/OrderServicesApplication.java
@@ -0,0 +1,15 @@
+package com.luizbarros.order_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServicesApplication.class, args);
+		
+		System.out.println("System ON!");
+	}
+
+}
