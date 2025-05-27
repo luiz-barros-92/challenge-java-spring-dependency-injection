c2aee6272e1aa9f6c37a4617d504aa8d3e1cbf1d
Luiz Barros <lck.barros@gmail.com>
2025-05-26 21:25:22 -0300

    finished the main class

--- src/main/java/com/luizbarros/order_services/OrderServicesApplication.java
+++ src/main/java/com/luizbarros/order_services/OrderServicesApplication.java
@@ -1,11 +1,21 @@
 package com.luizbarros.order_services;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.luizbarros.order_services.entities.Order;
+import com.luizbarros.order_services.service.OrderService;
+
 @SpringBootApplication
 public class OrderServicesApplication implements CommandLineRunner{
+	
+	@Autowired
+	OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderServicesApplication.class, args);		
@@ -13,6 +23,25 @@ public class OrderServicesApplication implements CommandLineRunner{
 	
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("System ON!");
+		
+		Locale.setDefault(Locale.US);
+		
+		Scanner sc = new Scanner(System.in);		
+		
+		System.out.println("---Order Data---");
+		
+		System.out.print("Code: ");
+		Integer code = sc.nextInt();		
+		
+		System.out.print("Basic value: ");
+		Double basic = sc.nextDouble();
+		
+		System.out.print("Discount(%): ");
+		Double discount = sc.nextDouble();
+		
+		
+		Order order = new Order(code, basic, discount);
+		
+		System.out.printf("%nOrder code: %d%nTotal value: $%.2f%n", order.getCode(), orderService.total(order));
 	}
 }
